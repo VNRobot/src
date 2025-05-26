9131bf441735a07a3847d42281a95be117f40bc8
Slava Nasonov <slavanasonov@shaw.ca>
2025-05-26 14:51:28 -0700

    hexaBug calculated leg

--- hexaBug/steps.ino
+++ hexaBug/steps.ino
@@ -15,19 +15,23 @@ struct phase {
 };
 // array size
 char arraySize = 43;
+// center of array
+char centerArray = 22;
+// leg speed const
+char speedConst = 4;
 // walking 43 points. center point is 22
-//          shift      -110  -105  -100   -95   -90   -85   -80   -75   -70   -65   -60   -55   -50   -45   -40   -35   -30   -25   -20   -15   -10    -5     0     5    10    15    20    25    30    35    40    45    50    55    60    65    70    75    80    85    90    95   100
-char m1WalkSt[43]   = {  22,   22,   22,   22,   22,   22,   22,   22,   23,   24,   25,   26,   27,   28,   30,   31,   33,   35,   37,   39,   41,   43,   45,   47,   50,   53,   56,   58,   61,   64,   68,   71,   74,   77,   81,   84,   88,   92,   97,  102,  108,  116,  126};
-char m2WalkSt[43]   = { 118,  112,  107,  102,   98,   94,   91,   87,   84,   81,   78,   75,   72,   69,   66,   63,   60,   58,   55,   52,   50,   48,   45,   42,   40,   38,   36,   34,   33,   31,   29,   27,   26,   25,   24,   23,   23,   22,   22,   22,   22,   22,   22};
-//  lifted  shift      -110  -105  -100   -95   -90   -85   -80   -75   -70   -65   -60   -55   -50   -45   -40   -35   -30   -25   -20   -15   -10    -5     0     5    10    15    20    25    30    35    40    45    50    55    60    65    70    75    80    85    90    95   100
-char m1LiftSt[43]   = {   5,    5,    5,    5,    5,    5,    6,    6,    7,    8,   10,   11,   12,   13,   15,   17,   19,   21,   23,   25,   28,   30,   32,   34,   38,   40,   44,   46,   50,   52,   56,   60,   64,   68,   70,   74,   78,   82,   86,   90,   94,   98,  103};
-char m2LiftSt[43]   = { 105,  100,   96,   92,   88,   84,   82,   78,   75,   72,   70,   66,   63,   60,   57,   54,   51,   48,   45,   43,   40,   37,   34,   31,   29,   26,   24,   22,   20,   18,   16,   14,   12,   10,    9,    8,    7,    6,    6,    5,    5,    5,    5};
-// lifted high shift   -110  -105  -100   -95   -90   -85   -80   -75   -70   -65   -60   -55   -50   -45   -40   -35   -30   -25   -20   -15   -10    -5     0     5    10    15    20    25    30    35    40    45    50    55    60    65    70    75    80    85    90    95   100
-char m1LiftH1[43]   = { -12,  -12,  -12,  -12,  -12,  -12,  -11,  -11,  -10,   -9,   -7,   -6,   -5,   -3,    0,    2,    4,    6,    8,   12,   14,   17,   20,   23,   26,   30,   33,   36,   40,   43,   47,   50,   54,   58,   62,   65,   69,   73,   77,   81,   86,   89,   94};
-char m2LiftH1[43]   = {  98,   95,   92,   88,   85,   82,   78,   75,   72,   68,   65,   62,   58,   55,   51,   48,   44,   41,   38,   34,   31,   28,   24,   21,   18,   15,   12,    9,    6,    4,    2,   -1,   -3,   -5,   -6,   -7,   -8,  -10,  -11,  -12,  -12,  -12,  -12};
-//  lift highest shift -110  -105  -100   -95   -90   -85   -80   -75   -70   -65   -60   -55   -50   -45   -40   -35   -30   -25   -20   -15   -10    -5     0     5    10    15    20    25    30    35    40    45    50    55    60    65    70    75    80    85    90    95   100
-char m1LiftH2[43]   = { -30,  -30,  -30,  -30,  -30,  -30,  -29,  -29,  -28,  -27,  -26,  -25,  -24,  -22,  -19,  -17,  -14,  -11,   -9,   -5,   -1,    1,    6,   10,   14,   18,   23,   26,   31,   35,   38,   43,   48,   52,   56,   60,   64,   69,   73,   77,   81,   85,   89};
-char m2LiftH2[43]   = {  97,   93,   90,   86,   83,   79,   76,   73,   70,   66,   62,   58,   55,   51,   47,   43,   38,   35,   31,   27,   23,   19,   15,   10,    7,    3,   -1,   -4,   -9,  -11,  -14,  -17,  -20,  -22,  -24,  -26,  -27,  -28,  -29,  -29,  -29,  -29,  -29};
+//            shift      -110  -105  -100   -95   -90   -85   -80   -75   -70   -65   -60   -55   -50   -45   -40   -35   -30   -25   -20   -15   -10    -5     0     5    10    15    20    25    30    35    40    45    50    55    60    65    70    75    80    85    90    95   100
+//char m1WalkSt[43]   = {  22,   22,   22,   22,   22,   22,   22,   22,   23,   24,   25,   26,   27,   28,   30,   31,   33,   35,   37,   39,   41,   43,   45,   47,   50,   53,   56,   58,   61,   64,   68,   71,   74,   77,   81,   84,   88,   92,   97,  102,  108,  116,  126};
+//char m2WalkSt[43]   = { 118,  112,  107,  102,   98,   94,   91,   87,   84,   81,   78,   75,   72,   69,   66,   63,   60,   58,   55,   52,   50,   48,   45,   42,   40,   38,   36,   34,   33,   31,   29,   27,   26,   25,   24,   23,   23,   22,   22,   22,   22,   22,   22};
+//    lifted  shift      -110  -105  -100   -95   -90   -85   -80   -75   -70   -65   -60   -55   -50   -45   -40   -35   -30   -25   -20   -15   -10    -5     0     5    10    15    20    25    30    35    40    45    50    55    60    65    70    75    80    85    90    95   100
+//char m1LiftSt[43]   = {   5,    5,    5,    5,    5,    5,    6,    6,    7,    8,   10,   11,   12,   13,   15,   17,   19,   21,   23,   25,   28,   30,   32,   34,   38,   40,   44,   46,   50,   52,   56,   60,   64,   68,   70,   74,   78,   82,   86,   90,   94,   98,  103};
+//char m2LiftSt[43]   = { 105,  100,   96,   92,   88,   84,   82,   78,   75,   72,   70,   66,   63,   60,   57,   54,   51,   48,   45,   43,   40,   37,   34,   31,   29,   26,   24,   22,   20,   18,   16,   14,   12,   10,    9,    8,    7,    6,    6,    5,    5,    5,    5};
+//   lifted high shift   -110  -105  -100   -95   -90   -85   -80   -75   -70   -65   -60   -55   -50   -45   -40   -35   -30   -25   -20   -15   -10    -5     0     5    10    15    20    25    30    35    40    45    50    55    60    65    70    75    80    85    90    95   100
+//char m1LiftH1[43]   = { -12,  -12,  -12,  -12,  -12,  -12,  -11,  -11,  -10,   -9,   -7,   -6,   -5,   -3,    0,    2,    4,    6,    8,   12,   14,   17,   20,   23,   26,   30,   33,   36,   40,   43,   47,   50,   54,   58,   62,   65,   69,   73,   77,   81,   86,   89,   94};
+//char m2LiftH1[43]   = {  98,   95,   92,   88,   85,   82,   78,   75,   72,   68,   65,   62,   58,   55,   51,   48,   44,   41,   38,   34,   31,   28,   24,   21,   18,   15,   12,    9,    6,    4,    2,   -1,   -3,   -5,   -6,   -7,   -8,  -10,  -11,  -12,  -12,  -12,  -12};
+//    lift highest shift -110  -105  -100   -95   -90   -85   -80   -75   -70   -65   -60   -55   -50   -45   -40   -35   -30   -25   -20   -15   -10    -5     0     5    10    15    20    25    30    35    40    45    50    55    60    65    70    75    80    85    90    95   100
+//char m1LiftH2[43]   = { -30,  -30,  -30,  -30,  -30,  -30,  -29,  -29,  -28,  -27,  -26,  -25,  -24,  -22,  -19,  -17,  -14,  -11,   -9,   -5,   -1,    1,    6,   10,   14,   18,   23,   26,   31,   35,   38,   43,   48,   52,   56,   60,   64,   69,   73,   77,   81,   85,   89};
+//char m2LiftH2[43]   = {  97,   93,   90,   86,   83,   79,   76,   73,   70,   66,   62,   58,   55,   51,   47,   43,   38,   35,   31,   27,   23,   19,   15,   10,    7,    3,   -1,   -4,   -9,  -11,  -14,  -17,  -20,  -22,  -24,  -26,  -27,  -28,  -29,  -29,  -29,  -29,  -29};
 // lift high flags. 2 - lift high, 1 - lift, 0 - walk
 //char mLiftHFlag[36]   = { 3, 3,  3,  3,  2,  1,  1,  1,  1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 3};
 char mLiftHFlag[36]     = { 3, 3,  3,  3,  2,  1,  0,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 3};
@@ -36,8 +40,6 @@ char mTouchFlag[36]     = { 0, 0,  0,  0,  0,  2,  2,  1,  1, 1, 1, 1, 1, 1, 1, 
 // walking pointers
 char mPointWalk[36]     = { 0,-5,-10,-15,-14,-13,-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,10, 5};
 char mPointWalkSlow[36] = { 0,-3, -6, -8, -7, -7, -6, -6, -5,-5,-4,-4,-3,-3,-2,-2,-1,-1, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 6, 3};
-// center of array
-// m_forwardCenterServo = 22; // bigger the number more weight on front
 //
 char forwardBallance = 0;
 // walking mode
@@ -48,6 +50,8 @@ char speedL = 0;
 char speedR = 0;
 // leg phase
 phase legPhase = {0, 0, 0, 0, 0, 0};
+// leg hight default 125
+phase legHight = {125, 125, 125, 125, 125, 125};
 // legset touched
 touch touchState = {false, false, false};
 touch holeState = {false, false, false};
@@ -113,78 +117,72 @@ char _getPoint(unsigned char counter, char speedValue) {
 }
 
 // set walk step value
-motors _setWalkStep(char pointBuffer, unsigned char counter, char speedValue) {
+motors _setWalkStep(char pointBuffer, unsigned char counter, char speedValue, unsigned char hight) {
+  unsigned char lift = 0;
   // motors values for 2 motors
   motors legStep = {0, 0};
   if (walkingMode) {
     switch (mLiftHFlag[counter]) {
       case 0:
       {
-        legStep.motor1 = m1WalkSt[pointBuffer];
-        legStep.motor2 = m2WalkSt[pointBuffer];
+        lift = 0;
       }
       break;
       case 1:
       {
-        legStep.motor1 = (m1LiftSt[pointBuffer] + m1WalkSt[pointBuffer]) / 2;
-        legStep.motor2 = (m2LiftSt[pointBuffer] + m2WalkSt[pointBuffer]) / 2;
+        lift = 5;
       }
       break;
       case 2:
       {
-        legStep.motor1 = m1LiftSt[pointBuffer];
-        legStep.motor2 = m2LiftSt[pointBuffer];
+        lift = 15;
       }
       break;
       case 3:
       {
         if (speedValue == 2) {
-          legStep.motor1 = m1LiftH2[pointBuffer];
-          legStep.motor2 = m2LiftH2[pointBuffer];
+        lift = 35;
         } else {
-          legStep.motor1 = m1LiftH1[pointBuffer];
-          legStep.motor2 = m2LiftH1[pointBuffer];
+        lift = 25;
         }
       }
       break;
       default:
       break;
     }
-  } else {
-    // do not apply lifting
-    legStep.motor1 = m1WalkSt[pointBuffer];
-    legStep.motor2 = m2WalkSt[pointBuffer];
   }
+  legStep.motor1 = _calculateMotorAngle((int)(hight - lift), (centerArray - pointBuffer) * speedConst);
+  legStep.motor2 = _calculateMotorAngle((int)(hight - lift), (pointBuffer - centerArray) * speedConst);
   return legStep;
 }
 
 // get servo motor walk values
 motors getWalkStep(unsigned char counter) {
-  return _setWalkStep(_getPoint(counter, speed), counter, speed);
+  return _setWalkStep(_getPoint(counter, speed), counter, speed, 125);
 }
 // get servo motor walk values FL
 motors getWalkStepFL(void) {
-  return _setWalkStep(_getPoint(legPhase.FL, speedL), legPhase.FL, speedL);
+  return _setWalkStep(_getPoint(legPhase.FL, speedL), legPhase.FL, speedL, legHight.FL);
 }
 // get servo motor walk values FR
 motors getWalkStepFR(void) {
-  return _setWalkStep(_getPoint(legPhase.FR, speedR), legPhase.FR, speedR);
+  return _setWalkStep(_getPoint(legPhase.FR, speedR), legPhase.FR, speedR, legHight.FR);
 }
 // get servo motor walk values SL
 motors getWalkStepSL(void) {
-  return _setWalkStep(_getPoint(legPhase.SL, speedL), legPhase.SL, speedL);
+  return _setWalkStep(_getPoint(legPhase.SL, speedL), legPhase.SL, speedL, legHight.SL);
 }
 // get servo motor walk values SR
 motors getWalkStepSR(void) {
-  return _setWalkStep(_getPoint(legPhase.SR, speedR), legPhase.SR, speedR);
+  return _setWalkStep(_getPoint(legPhase.SR, speedR), legPhase.SR, speedR, legHight.SR);
 }
 // get servo motor walk values RL
 motors getWalkStepRL(void) {
-  return _setWalkStep(_getPoint(legPhase.RL, speedL), legPhase.RL, speedL);
+  return _setWalkStep(_getPoint(legPhase.RL, speedL), legPhase.RL, speedL, legHight.RL);
 }
 // get servo motor walk values RR
 motors getWalkStepRR(void) {
-  return _setWalkStep(_getPoint(legPhase.RR, speedR), legPhase.RR, speedR);
+  return _setWalkStep(_getPoint(legPhase.RR, speedR), legPhase.RR, speedR, legHight.RR);
 }
 
 // set mode and speed
@@ -304,4 +302,23 @@ bool updateSurfaceSteps(touch touchIn) {
   // mTouchFlag  legPhase.FL  legPhase.FR  touchState.set1  holeState.set1  touchIn.set1
   //  mTouchFlag[legPhase.FL] = mTouchFlag[legPhase.FR]
   // 0 - lifted, 1 - down, 2 - unknown
+}
+
+// calculate motor angles
+char _calculateMotorAngle(int Hval, int Sval) {
+  // calculate Lvalue and angle c
+  float Lval = (float)Hval;
+  float AngleC = 0;
+  float AngleB = 0;
+  float AngleA = 0;
+  Sval -= 16;
+  Lval = sqrt(Hval * Hval + Sval * Sval);
+  AngleC = (acos(Hval / Lval) * 180) / 3.14;
+  if (Sval < 0) {
+    AngleC = -AngleC;
+  }
+  //AngleB = (acos((Lval * Lval + 70 * 70 - 100 * 100) / (2 * Lval * 70)) * 180) / 3.14;
+  AngleB = (acos((Lval * Lval - 5100) / (Lval * 140)) * 180) / 3.14;
+  AngleA = 90 - AngleB - AngleC;
+  return (char)AngleA;
 }
